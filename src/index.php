b20428e4a64294c27f205006d303da84000dbff7
Samara Ferreira <ferreirasamara.sf@gmail.com>
2020-12-08 17:38:56 -0300

    adjust home screen

--- src/index.php
+++ src/index.php
@@ -1,35 +1,3 @@
 <?php
-  require_once "database/Conn.php";
-  require_once "models/Pet.php";
-  require_once "models/User.php";
-  require_once "services/PetService.php";
-  require_once "services/UserService.php";
-  require_once "./services/ImageUpload.php";
-
-  $db = new Conn('localhost','pets','root', '');
-
-  $folderUsers = '../tmp/users/';
-  $entity = 'users';
-
-  $imagePath = upload($folderUsers, $entity);
-  
-  if ($imagePath !== '') {
-    $name = $_POST['name'];
-    $birthDate = $_POST['birthDate'];
-    $email = $_POST['email'];
-    $address = $_POST['address'];
-
-    $user = new User;
-    $user->setName($name);
-    $user->setBirthDate($birthDate);
-    $user->setEmail($email);
-    $user->setPhoto($imagePath);
-    $user->setAddress($address);
-    print_r($user->getPhoto());
-
-    $service = new UserService($db, $user);
-    $userId = $service->save();
-  } else {
-    echo 'Falha ao salvar imagem.';
-  }
+  header ("location: http://localhost/pets/src/presentation/home");
 ?>
